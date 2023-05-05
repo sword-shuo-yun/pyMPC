f0e81570d969718d289208e3835d838328e6d49a
J.Chen <479461838@qq.com>
2023-05-05 17:42:44 +0800

    commit mpc kalman filter version

--- Python_others/mpc_kalman.py
+++ Python_others/mpc_kalman.py
@@ -2,20 +2,32 @@
 # -*- coding: UTF-8 -*-
 """
 @Project ：python_mpc 
-@File    ：MPC_test_wei.py
+@File    ：MPC_test1.py
 @Author  ：梁伟
 @mail    ：1461334995@qq.com
-@Date    ：2023/4/20 16:11 
+@Date    ：2023/4/20 16:11
 """
+import random
+
 import matplotlib.pyplot as plt
 import scipy.linalg
 from cvxopt import solvers, matrix
 
 from pyMPC.kalman import *
 
-A = np.array([[1, 1], [-1, 2]])
-B = np.array([[1, 1], [1, 2]])
-C = np.array([[1, 0], [0, 1]])
+A = np.array([[0.8725, 0, 0, 0],
+              [0, 0.8187, 0, 0],
+              [0, 0, 0.8826, 0],
+              [0, 0, 0, 0.8317]])
+B = np.array([[3.5056, 0],
+              [1.6994, 0],
+              [0, 1.762],
+              [0, 0.8562]])
+C = np.array([[0.1059, 0, -0.0403, 0],
+              [0, 0.0373, 0, -0.0393]])
+# A = np.array([[1, 1], [-1, 2]])
+# B = np.array([[1, 1], [1, 2]])
+# C = np.array([[1, 0], [0, 1]])
 
 D = np.zeros((2, 2))
 
@@ -27,7 +39,7 @@ Q = np.array([[10, 0], [0, 10]])
 F = np.array([[1, 0], [0, 1]])
 R = np.array([[1, 0], [0, 1]])
 
-r = np.array([[10], [5]])
+r = np.array([[0], [0]])
 N = 5
 
 
@@ -64,25 +76,25 @@ def prediction(H, E, p):
 E, H = cal_matrices(A, B, C, D, Q, R, F, N)
 
 C_pinv = np.linalg.pinv(C)
-k_step = 100
+k_step = 500
+
+# Basic Kalman filter design
+Q_kal = 10 * np.eye(n)
+R_kal = np.eye(m)
+L, P, W = kalman_design_simple(A, B, C, D, Q_kal, R_kal,
+                               type = 'predictor')
+x0 = np.array([[0], [0], [0], [0]])
+x0_est = x0
+KF = LinearStateEstimator(x0_est, A, B, C, D, L)
+
 Y_k = np.zeros((m, k_step))
 X_k = np.zeros((n, k_step))
 U_k = np.zeros((p, k_step))
 Y_k[:, 0] = np.array([10, -10])
 u0 = np.array([[[0], [0]]])
-
-# # Basic Kalman filter design
-# Q_kal = 10 * np.eye(n)
-# R_kal = np.eye(m)
-# L, P, W = kalman_design_simple(A, B, C, D, Q_kal, R_kal,
-#                                type = 'predictor')
-# x0 = np.array([[0], [0]])
-# x0_est = x0
-# KF = LinearStateEstimator(x0_est, A, B, C, D, L)
 r_k = np.zeros((m, k_step))
-r_k[:, 0:1] = r
 
-for k in range(1, k_step):
+for k in range(k_step - 1):
     if k > 20:
         r = np.array([[5], [0]])
     if k > 40:
@@ -91,26 +103,25 @@ for k in range(1, k_step):
         r = np.array([[10], [-5]])
     if k > 80:
         r = np.array([[0], [0]])
-    r_k[:, k - 1:k] = r
-    y_delta = Y_k[:, k - 1:k] - r
-    y_delta = y_delta.reshape(m, 1)
-    # y_meas = Y_k[:, k:k + 1] - r
-    # x_est_k = X_k[:, k:k + 1]
-    x_delta = C_pinv @ y_delta
-    T = x_delta.T @ E
+    r_k[:, k:k + 1] = r
+    y_meas = Y_k[:, k:k + 1] - r
+    x_est_k_pre = KF.x
+    T = x_est_k_pre.T @ E
     T = matrix(T.T)
     H = matrix(H)
     u_k_delta = prediction(H, T, p)
 
-    # KF.update(y_meas)
-    # KF.predict(u_k_delta)
+    KF.update(y_meas)
+    x_est_k = KF.x
+    KF.predict(u_k_delta)
 
-    x_1_delta = A @ x_delta + B @ u_k_delta
+    x_1_delta = A @ x_est_k + B @ u_k_delta
     y_1_delta = C @ x_1_delta
 
-    Y_k[:, k:k + 1] = y_1_delta + r.reshape(m, 1)
-    u0 = u0 if k < 2 else U_k[:, k - 2:k - 1]
-    U_k[:, k - 1:k] = u0 + u_k_delta
+    Y_k[:, k + 1:k + 2] = y_1_delta + r + np.array(
+            [[random.uniform(-1, 1)], [random.uniform(-1, 1)]])
+    u0 = u0 if k == 0 else U_k[:, k - 1:k]
+    U_k[:, k:k + 1] = u_k_delta + u0
 
 fig = plt.figure
 ax1 = plt.subplot(211)
